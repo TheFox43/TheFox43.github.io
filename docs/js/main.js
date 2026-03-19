a41520e7390a42ed27fb672fd7969d00d913f89e
Francesco Volpe <francesco.volpe1998@gmail.com>
2026-03-19 18:29:48 +0100

    fixed nav window and titles

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,15 @@
-const toggle = document.getElementById("menu-toggle");
-const menu = document.querySelector(".menu");
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("menu-toggle");
+    const menu = document.querySelector(".menu");
 
-toggle.addEventListener("click", () => {
-    menu.classList.toggle("active");
+    toggle.addEventListener("click", (e) => {
+        e.preventDefault();
+        menu.classList.toggle("active");
+    });
+
+    document.addEventListener("click", (e) => {
+        if (!menu.contains(e.target)) {
+            menu.classList.remove("active");
+        }
+    });
 });
